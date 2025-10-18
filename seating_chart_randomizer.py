970a51668db9431e685ed9b2e5bdfa3a2ec42d05
jamesegre <jamesegre@leeschools.net>
2025-10-18 00:34:12 -0400

    Add files via upload

--- seating_chart_randomizer.py
+++ seating_chart_randomizer.py
@@ -10,29 +10,6 @@ def create_list_from_text(text):
 
     return value
 
-def create_num_list(text):
-    value = 0
-    value_list = []
-    
-    for i in range(len(create_list_from_text(text))):
-        value += 1
-        value_list.append(value)
-
-    random.shuffle(value_list)
-
-    return value_list
-
-def create_seat_chart(roster,number_list):
-    seating_chart = {}
-    for x,y in enumerate(roster):
-        seating_chart[f'{y}'] = number_list[x]
-
-    return seating_chart
-
-def random_seat_chart(text):
-    return create_seat_chart(create_list_from_text(text),
-                                  create_num_list(text))
-
 def create_list_in_list(text_list):
     #if list is student name and kagan number
     return_list = []
@@ -42,17 +19,47 @@ def create_list_in_list(text_list):
 
     return return_list
 
+def text_list_list(text):
+    #if list is student name and kagan number
+    value = create_list_from_text(text)
+
+    return create_list_in_list(value)
+
+##def create_num_list(text):
+##    value = 0
+##    value_list = []
+##    
+##    for i in range(len(create_list_from_text(text))):
+##        value += 1
+##        value_list.append(value)
+##
+##    random.shuffle(value_list)
+##
+##    return value_list
+
+##def create_seat_chart(roster,number_list):
+##    seating_chart = {}
+##    for x,y in enumerate(roster):
+##        seating_chart[f'{y}'] = number_list[x]
+##
+##    return seating_chart
+
+##def random_seat_chart(text):
+##    return create_seat_chart(create_list_from_text(text),
+##                                  create_num_list(text))
+
+
 def kagan_random_seats(text_list):
-    ones = [5,7,13,15,21,23]
+    ones = [5,7,13,15,21,23,29,31]
     random.shuffle(ones)
     
-    twos = [1,3,9,11,17,19]
+    twos = [1,3,9,11,17,19,25,27]
     random.shuffle(twos)
     
-    threes =[2,4,10,12,18,20]
+    threes =[2,4,10,12,18,20,26,28]
     random.shuffle(threes)
     
-    fours = [6,8,14,16,22,24]
+    fours = [6,8,14,16,22,24,30,32]
     random.shuffle(fours)
 
     ones_counter = 0
@@ -61,25 +68,114 @@ def kagan_random_seats(text_list):
     fours_counter = 0
 
     for i in text_list:
-        print(i)
         if int(i[1]) == 1:
-            if ones_counter < 6:
+            if ones_counter < 8:
                 i.append(ones[ones_counter])
                 ones_counter += 1
-            elif twos_counter < 6:
+            elif twos_counter < 8:
                 i.append(twos[twos_counter])
                 twos_counter += 1
+            elif threes_counter < 8:
+                i.append(threes[threes_counter])
+                threes_counter += 1
+            elif fours_counter < 8:
+                i.append(fours[fours_counter])
+                fours_counter += 1 
         elif int(i[1]) == 2:
-            if twos_counter < 6:
+            if ones_counter < 8:
+                i.append(ones[ones_counter])
+                ones_counter += 1
+            elif twos_counter < 8:
                 i.append(twos[twos_counter])
-            twos_counter += 1
+                twos_counter += 1
+            elif threes_counter < 8:
+                i.append(threes[threes_counter])
+                threes_counter += 1
+            elif fours_counter < 8:
+                i.append(fours[fours_counter])
+                fours_counter += 1 
         elif int(i[1]) == 3:
-            i.append(threes[threes_counter])
-            threes_counter += 1
+            if ones_counter < 8:
+                i.append(ones[ones_counter])
+                ones_counter += 1
+            elif twos_counter < 8:
+                i.append(twos[twos_counter])
+                twos_counter += 1
+            elif threes_counter < 8:
+                i.append(threes[threes_counter])
+                threes_counter += 1
+            elif fours_counter < 8:
+                i.append(fours[fours_counter])
+                fours_counter += 1 
         elif int(i[1]) == 4:
-            i.append(fours[fours_counter])
-            fours_counter += 1
+            if ones_counter < 8:
+                i.append(ones[ones_counter])
+                ones_counter += 1
+            elif twos_counter < 8:
+                i.append(twos[twos_counter])
+                twos_counter += 1
+            elif threes_counter < 8:
+                i.append(threes[threes_counter])
+                threes_counter += 1
+            elif fours_counter < 8:
+                i.append(fours[fours_counter])
+                fours_counter += 1 
 
-    return text_list
+    return text_list#, kagan_diagnostics(text_list)
 
+def kagan_diagnostics(text_list):
+    table_one = [5,1,2,6]
+    t1_counter = 0
+    
+    table_two = [7,3,4,8]
+    t2_counter = 0
+
+    table_three = [13,9,10,14]
+    t3_counter = 0
+
+    table_four = [15,11,12,16]
+    t4_counter = 0
+    
+    table_five = [21,17,18,22]
+    t5_counter = 0
+
+    table_six = [23,19,20,24]
+    t6_counter = 0
+
+    table_seven = [29,25,26,30]
+    t7_counter = 0
+
+    table_eight = [31,27,28,32]
+    t8_counter = 0
+    
+    for i in text_list:
+        try:
+            if i[2] in table_one:
+                t1_counter += 1
+            elif i[2]  in table_two:
+                t2_counter += 1
+            elif i[2]  in table_three:
+                t3_counter += 1
+            elif i[2]  in table_four:
+                t4_counter += 1
+            elif i[2]  in table_five:
+                t5_counter += 1
+            elif i[2]  in table_six:
+                t6_counter += 1
+            elif i[2]  in table_seven:
+                t7_counter += 1
+            elif i[2]  in table_eight:
+                t8_counter += 1
+        except IndexError:
+            pass
+    print(f'''
+            Table One has   {t1_counter} students
+            Table Two has   {t2_counter} students
+            Table Three has {t3_counter} students
+            Table Four has  {t4_counter} students
+            Table Five has  {t5_counter} students
+            Table Six has   {t6_counter} students
+            Table Seven has {t7_counter} students
+            Table Eight has {t8_counter} students
+            ''')
 
